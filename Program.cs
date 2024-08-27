e075e8a42650815a49e0f09f052c70cbc3fde051
Cobra 陳俊宇 <cobra20621@gmail.com>
2024-08-27 19:22:37 +0800

    Integrate MudBlazor for UI components and enhance functionality

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Resume_Editor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
 
 await builder.Build().RunAsync();
 
